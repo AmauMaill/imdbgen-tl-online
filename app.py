df7f9525046a4ae924e637df51ccdeb41fba6a5d
AmauMaill <amaury.maill@gmail.com>
2022-07-09 23:02:59 +0200

    gradio app

--- app.py
+++ app.py
@@ -2,19 +2,17 @@
 Source: https://towardsdatascience.com/how-to-deploy-a-machine-learning-ui-on-heroku-in-5-steps-b8cd3c9208e6
 """
 
-from fastai.vision.all import load_learner
+from fastai.text.all import load_learner
 import gradio as gr
 
 def predict(inp):
     model = load_learner("./export.pkl")
-    labels = model.dls.vocab
-    prediction = model.predict(inp)
-    confidences = {labels[i]: float(prediction[2][i]) for i in range(2)}
-    return confidences
+    prediction = model.predict(inp, 32, temperature=1)
+    return prediction
 
 gr.Interface(
     fn=predict,
-    inputs=gr.inputs.Image(type="filepath"),
-    outputs=gr.outputs.Label(num_top_classes=2),
-    examples=["./data/kanelbullar.jpg", "./data/semlor.jpg"]
-).launch()
+    inputs=gr.inputs.Textbox(),
+    outputs=gr.outputs.Textbox(),
+    examples=["The film was", "What a waste", "I have to admit"]
+).launch(inbrowser=True)
